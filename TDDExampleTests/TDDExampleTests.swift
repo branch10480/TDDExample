a6163f46a55ae738e8b639b91810e7fb54221191
branch10480 <toshiharu.imaeda@gmail.com>
2021-04-07 22:43:16 +0900

    失敗するテストを書く

--- TDDExampleTests/TDDExampleTests.swift
+++ TDDExampleTests/TDDExampleTests.swift
@@ -5,29 +5,21 @@
 //  Created by branch10480 on 2021/04/07.
 //
 
-import XCTest
+import Quick
+import Nimble
+
 @testable import TDDExample
 
-class TDDExampleTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class ArticleListSpec: QuickSpec {
+    
+    override func spec() {
+        it("タイトルが表示されている") {
+            let vc = ArticleListViewController()
+            let window = UIWindow()
+            window.rootViewController = vc
+            window.makeKeyAndVisible()
+            
+            expect(vc.titleLabel.text).to(equal("記事タイトル"))
         }
     }
-
 }
